b553651005101de3c8b67f8694c7ebc172ddeb56
himanshu010 <himanshuaswal@yahoo.com>
2020-06-26 16:12:57 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,135 +1,5 @@
-// function init() {
-//   var canvas = document.getElementById("mycanvas");
-//   W = canvas.width = 1000;
-//   H = canvas.height = 1000;
-//   pen = canvas.getContext("2d");
-//   cs = 67;
-//   game_over = false;
-//   food = getRandomFood();
-
-//   snake = {
-//     init_length: 5,
-//     color: "blue",
-//     cells: [],
-//     direction: "right",
-//     createSnake: function () {
-//       for (var i = this.init_length; i > 0; i--) {
-//         this.cells.push({ x: i, y: 0 });
-//       }
-//     },
-
-//     drawSnake: function () {
-//       var gradient1 = pen.createLinearGradient(50, 10, 700, 10);
-//       gradient1.addColorStop(0, "aqua");
-//       gradient1.addColorStop(1, "rgba(20, 00, 205, 0.8)");
-//       pen.fillStyle = gradient1;
-//       for (var i = 0; i < this.cells.length; i++) {
-//         pen.fillRect(
-//           this.cells[i].x * cs,
-//           this.cells[i].y * cs,
-//           cs - 2,
-//           cs - 2
-//         );
-//       }
-//     },
-
-//     updateSnake: function () {
-//       var headX = this.cells[0].x;
-//       var headY = this.cells[0].y;
-
-//       if (headX === food.x && headY === food.y) {
-//         console.log("Food Eaten");
-//         food = getRandomFood();
-//       } else {
-//         this.cells.pop();
-//       }
-
-//       var nextX, nextY;
-//       if (this.direction === "right") {
-//         nextX = headX + 1;
-//         nextY = headY;
-//       } else if (this.direction === "left") {
-//         nextX = headX - 1;
-//         nextY = headY;
-//       } else if (this.direction === "down") {
-//         nextX = headX;
-//         nextY = headY + 1;
-//       } else {
-//         nextX = headX;
-//         nextY = headY - 1;
-//       }
-
-//       this.cells.unshift({ x: nextX, y: nextY });
-//       var last_X = Math.round(W / cs);
-//       var last_Y = Math.round(H / cs);
-
-//       if (
-//         this.cells[0].y < 0 ||
-//         this.cells[0].y > last_Y ||
-//         this.cells[0].x < 0 ||
-//         this.cells[0].x > last_X
-//       ) {
-//         game_over = true;
-//       }
-//     },
-//   };
-//   snake.createSnake();
-
-//   function keypressed(e) {
-//     console.log(e);
-
-//     if (e.key === "ArrowRight" || e.key === "d" || e.key === "D") {
-//       snake.direction = "right";
-//     } else if (e.key === "ArrowLeft" || e.key === "a" || e.key === "A") {
-//       snake.direction = "left";
-//     } else if (e.key === "ArrowDown" || e.key === "s" || e.key === "S") {
-//       snake.direction = "down";
-//     } else if (e.key === "ArrowUp" || e.key === "w" || e.key === "W") {
-//       snake.direction = "up";
-//     }
-
-//     // console.log(snake.direction);
-//   }
-
-//   document.addEventListener("keydown", keypressed);
-// }
-
-// function draw() {
-//   pen.clearRect(0, 0, W, H);
-//   snake.drawSnake();
-//   pen.fillStyle = food.color;
-//   pen.fillRect(food.x * cs, food.y * cs, cs, cs);
-// }
-
-// function getRandomFood() {
-//   var foodX = Math.round((Math.random() * (W - cs)) / cs);
-//   var foodY = Math.round((Math.random() * (H - cs)) / cs);
-
-//   var food = {
-//     x: foodX,
-//     y: foodY,
-//     color: "red",
-//   };
-
-//   return food;
-// }
-
-// function update() {
-//   snake.updateSnake();
-// }
-
-// function gameloop() {
-//   if (game_over === true) {
-//     clearInterval(f);
-//   }
-//   draw();
-//   update();
-// }
-
-// init();
-// gameloop();
-
-// var f = setInterval(gameloop, 100);
+var start = document.getElementById("start");
+var reset = document.getElementById("reset");
 
 function init() {
   canvas = document.getElementById("mycanvas");
@@ -138,6 +8,10 @@ function init() {
   cs = 66;
   game_over = false;
   score = 5;
+  var up = document.getElementById("up");
+  var left = document.getElementById("left");
+  var right = document.getElementById("right");
+  var down = document.getElementById("down");
 
   //Create a Image Object for food
   food_img = new Image();
@@ -167,12 +41,7 @@ function init() {
       gradient1.addColorStop(1, "rgba(20, 00, 205, 0.8)");
       pen.fillStyle = gradient1;
       for (var i = 0; i < this.cells.length; i++) {
-        pen.fillRect(
-          this.cells[i].x * cs,
-          this.cells[i].y * cs,
-          cs - 3,
-          cs - 3
-        );
+        pen.fillRect(this.cells[i].x * cs, this.cells[i].y * cs, cs, cs);
       }
     },
 
@@ -221,26 +90,55 @@ function init() {
       ) {
         game_over = true;
       }
+
+      var f = this.cells[0].x;
+      var k = this.cells[0].y;
+
+      for (var i = 1; i < this.cells.length; i++) {
+        if (f == this.cells[i].x && k == this.cells[i].y) {
+          console.log("game over");
+          game_over = true;
+          return;
+        }
+      }
     },
   };
 
   snake.createSnake();
   //Add a Event Listener on the Document Object
   function keyPressed(e) {
-    //Conditional Statments
-    if (e.key == "ArrowRight") {
+    if (e.key == "ArrowRight" && snake.direction !== "left") {
       snake.direction = "right";
-    } else if (e.key == "ArrowLeft") {
+    } else if (e.key == "ArrowLeft" && snake.direction !== "right") {
       snake.direction = "left";
-    } else if (e.key == "ArrowDown") {
+    } else if (e.key == "ArrowDown" && snake.direction !== "up") {
       snake.direction = "down";
-    } else {
+    } else if (e.key == "ArrowUp" && snake.direction !== "down") {
       snake.direction = "up";
     }
-    console.log(snake.direction);
   }
 
   document.addEventListener("keydown", keyPressed);
+  up.onclick = function () {
+    if (snake.direction !== "down") {
+      snake.direction = "up";
+    }
+  };
+  left.onclick = function () {
+    if (snake.direction !== "right") {
+      snake.direction = "left";
+    }
+  };
+  right.onclick = function () {
+    if (snake.direction !== "left") {
+      snake.direction = "right";
+    }
+  };
+  down.onclick = function () {
+    if (snake.direction !== "up") {
+      snake.direction = "down";
+    }
+  };
 }
 
 function draw() {
@@ -275,17 +173,30 @@ function getRandomFood() {
   };
   return food;
 }
-
+// start1.onclick = function () {
+//   var f = setInterval(gameloop, 100);
+// };
+var go = document.getElementById("gameover");
+go.style.display = "none";
 function gameloop() {
   if (game_over == true) {
-    clearInterval(f);
-    alert("Game Over");
+    clearInterval(gameloop);
+    // game_over_true();
+    go.style.display = "block";
+
     return;
   }
   draw();
   update();
 }
-
+reset.style.display = "none";
+reset.onclick = function () {
+  init();
+};
 init();
-
-var f = setInterval(gameloop, 100);
+start.onclick = function () {
+  // init();
+  var f = setInterval(gameloop, 100);
+  start.style.display = "none";
+  reset.style.display = "block";
+};
